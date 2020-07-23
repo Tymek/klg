e5dad814af43e131a70f133b7d36519010a48f49
Tymoteusz Czech <mail@tymek.cz>
2020-07-23 15:37:19 +0200

    contact page

---
+++ src/pages/kontakt.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+const KontaktPage = () => (
+  <Layout>
+    <SEO title="Kontakt" />
+  </Layout>
+)
+
+export default KontaktPage
