93699282d0a1e7c1278f625e0e07fdfdf9238b89
Tymoteusz Czech <mail@tymek.cz>
2020-11-14 17:25:09 +0100

    404 update

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,12 +2,19 @@ import React from "react"
 
 import Layout from "../components/Layout/Layout"
 import SEO from "../components/seo"
+import Wrapper from "../components/Wrapper"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>Nie znaleziono</h1>
-    <p>Bardzo mi przykro, ale nie ma tu niczego ciekawego.</p>
+    <Wrapper className="py-12">
+      <h1 className="text-xxl font-bold">
+        <span className="text-fuchsia">404</span> Nie Znalezionko!
+      </h1>
+      <p className="text-right">
+        Bardzo mi przykro, ale nie ma tu niczego ciekawego&hellip;
+      </p>
+    </Wrapper>
   </Layout>
 )
 
