f940eba06771d056b79ad06aa2e637a3218fb93a
Tymoteusz Czech <mail@tymek.cz>
2020-11-14 16:33:38 +0100

    customize home page

--- src/components/Layout/components/MenuLink.tsx
+++ src/components/Layout/components/MenuLink.tsx
@@ -1,19 +1,29 @@
 import React, { FC, ComponentProps } from "react"
 import { Link } from "gatsby"
-import './MenuLink.css'
+import "./MenuLink.css"
 
-const MenuLink: FC<ComponentProps<typeof Link>> = ({ children, to}) => (
-  <Link to={to} className="relative menuLink inline-block">
-    <span className="pb-1 px-1">
-      {children}
-    </span>
-    <div
-      aria-hidden
-      className="absolute text-white bg-black h-full left-0 top-0"
-    >
-      <span className="px-1">{children}</span>
-    </div>
-  </Link>
-)
+const MenuLink: FC<ComponentProps<typeof Link>> = ({ children, to }) => {
+  const contents = (
+    <>
+      <span className="pb-1 px-1">{children}</span>
+      <div
+        aria-hidden
+        className="absolute text-white bg-black h-full left-0 top-0"
+      >
+        <span className="px-1">{children}</span>
+      </div>
+    </>
+  )
+
+  return to.startsWith("#") ? ( // Anchor link
+    <a href={to} className="relative menuLink inline-block">
+      {contents}
+    </a>
+  ) : (
+    <Link to={to} className="relative menuLink inline-block">
+      {contents}
+    </Link>
+  )
+}
 
 export default MenuLink
---
+++ src/containers/HomePage/components/Portfolio/Portfolio.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react"
+// import "./Portfolio.css"
+
+type PortfolioProps = {}
+
+const Portfolio: FC<PortfolioProps> = () => {
+  return <></>
+}
+
+export default Portfolio
