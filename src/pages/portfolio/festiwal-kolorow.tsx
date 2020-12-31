3ee1861b4f7ae84b8ea286116e5b9be4f269c183
Tymoteusz Czech <mail@tymek.cz>
2020-12-31 17:18:12 +0100

    festiwal - vectors

--- src/pages/portfolio/festiwal-kolorow.tsx
+++ src/pages/portfolio/festiwal-kolorow.tsx
@@ -10,6 +10,12 @@ import Image from "../../components/Image"
 import Cover from "../../components/Cover"
 import Grid, { Column } from "../../components/Grid"
 
+import svgBlob from "../../images/festiwal-kolorow/vector/1.svg"
+import svgTeardrop from "../../images/festiwal-kolorow/vector/2.svg"
+import svgDotA from "../../images/festiwal-kolorow/vector/dot-red.svg"
+import svgDotB from "../../images/festiwal-kolorow/vector/dot-yellow.svg"
+import svgDotC from "../../images/festiwal-kolorow/vector/dot-blue.svg"
+
 const ColorFestivalPage = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -112,6 +118,8 @@ const ColorFestivalPage = () => {
             </Column>
           </Grid>
         </Wrapper>
+
+        {/* Folder covers */}
         <div className="mb-16 xs:mb-24 lg:mb-32 xl:mb-48 xxl:mb-48">
           <Wrapper className="pt-24 lg:pt-32">
             <Grid>
@@ -121,7 +129,13 @@ const ColorFestivalPage = () => {
             </Grid>
           </Wrapper>
         </div>
-        <div className="mb-16 xs:mb-24 lg:mb-32 xl:mb-48 xxl:mb-48">
+
+        {/* Open booklet */}
+        <div className="relative mb-16 xs:mb-24 lg:mb-32 xl:mb-48 xxl:mb-48">
+          <img
+            src={svgTeardrop}
+            className="absolute w-1/3 top-0 left-0 transform -translate-x-4/5 rotate-45 z-10"
+          />
           <Wrapper>
             <Grid>
               <Column className="xs:col-start-2 xs:col-span-8 sm:col-start-3 sm:col-span-6">
@@ -129,40 +143,78 @@ const ColorFestivalPage = () => {
               </Column>
             </Grid>
           </Wrapper>
+          <img
+            src={svgDotA}
+            // className="absolute top-0 left-0 transform -translate-y-full z-20"
+            className="absolute bottom-0 transform translate-x-2/5 translate-y-3/5 z-20"
+            style={{ width: "11vw" }}
+          />
         </div>
 
-        <Wrapper className="mb-16 xs:mb-24 lg:mb-32 xl:mb-48 xxl:mb-48">
-          <Grid className="pb-16">
-            <Column className="xs:col-start-2 xs:col-span-8 lg:col-start-4 lg:col-span-3">
-              <P className="leading-relaxed">
-                Podręcznik dla nauczyciela składa się z lekcji, z których każda
-                przeznaczona jest na jeden dzień. Scenariusze lekcji podzielone
-                są na części oznaczone ikonkami. Margines na każdej ze stron to
-                miejsce na notatki nauczyciela.
-              </P>
-            </Column>
-          </Grid>
-        </Wrapper>
-
-        <div className="mb-16 xs:mb-24 lg:mb-32 xl:mb-48 xxl:mb-48">
-          <Wrapper>
-            <Grid>
-              <Column className="xs:col-start-1 xs:col-span-8 sm:col-start-2 sm:col-span-6">
-                <Image fluid={data.mockup3.childImageSharp.fluid} />
+        {/* Text */}
+        <div className="relative">
+          <img
+            src={svgBlob}
+            className="absolute top-0 left-0 transform -translate-y-1/3 -translate-x-1/5 z-10"
+            style={{ width: "22vw" }}
+          />
+          <Wrapper className="mb-16 xs:mb-24 lg:mb-32 xl:mb-48 xxl:mb-48">
+            <Grid className="pb-16">
+              <Column className="col-start-3 col-span-7 lg:col-start-4 lg:col-span-3">
+                <P className="text-sm xs:text-base leading-relaxed relative z-50">
+                  Podręcznik dla nauczyciela składa się z lekcji, z których
+                  każda przeznaczona jest na jeden dzień. Scenariusze lekcji
+                  podzielone są na części oznaczone ikonkami. Margines na każdej
+                  ze stron to miejsce na notatki nauczyciela.
+                </P>
               </Column>
             </Grid>
           </Wrapper>
         </div>
+
+        {/* Pages mockup 1 */}
+        <div className="relative mb-16 xs:mb-24 lg:mb-32 xl:mb-48 xxl:mb-48">
+          <div className="absolute w-1/3">
+            <img
+              src={svgDotC}
+              className="absolute top-0 right-0 transform -translate-x-full -translate-y-2/3 z-50"
+              style={{ width: "5vw" }}
+            />
+          </div>
+          <Wrapper>
+            <Grid>
+              <Column className="relative xs:col-start-1 xs:col-span-8 sm:col-start-2 sm:col-span-6 z-40">
+                <img
+                  src={svgTeardrop}
+                  className="absolute bottom-0 right-0 transform translate-x-2/5 translate-y-1/5 z-30"
+                  style={{ width: "20vw" }}
+                />
+                <Image
+                  fluid={data.mockup3.childImageSharp.fluid}
+                  className="z-40"
+                />
+                <img
+                  src={svgDotB}
+                  className="absolute top-1/2 right-0 transform -translate-y-full translate-x-1/2 z-50"
+                  style={{ width: "4vw" }}
+                />
+              </Column>
+            </Grid>
+          </Wrapper>
+        </div>
+
+        {/* Pages mockup 2 */}
         <div className="mb-16 xs:mb-24 lg:mb-32 xl:mb-48 xxl:mb-48">
           <Wrapper>
             <Grid>
-              <Column className="xs:col-start-1 xs:col-span-8 sm:col-start-2 sm:col-span-6">
+              <Column className="xs:col-start-1 xs:col-span-8 md:col-start-2 md:col-span-6">
                 <Image fluid={data.mockup4.childImageSharp.fluid} />
               </Column>
             </Grid>
           </Wrapper>
         </div>
 
+        {/* Poster */}
         <Wrapper>
           <Cover
             image={data.poster.childImageSharp.fluid}
