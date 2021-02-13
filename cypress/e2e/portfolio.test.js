18f5142cd35db0fb98107f4ef4bffd7e15e57bf2
Tymoteusz Czech <mail@tymek.cz>
2021-02-13 14:21:44 +0100

    fix e2e test

--- cypress/e2e/portfolio.test.js
+++ cypress/e2e/portfolio.test.js
@@ -1,14 +1,17 @@
 /// <reference types="Cypress" />
 
+const portfolioItemsCount = 11
+const portfolioToyItems = 4
+
 describe("Portfolio", () => {
   it("filters items", () => {
     cy.visit("/#portfolio")
-    cy.get('[data-test=portfolio-list]').children().its('length').should('eq', 9)
+    cy.get('[data-test=portfolio-list]').children().its('length').should('eq', portfolioItemsCount)
     cy.get('[data-test=portfolio-navigation] > :nth-child(1) a').should('not.have.class', 'active')
     cy.get('[data-test=portfolio-navigation] > :nth-child(1) a').click()
     cy.get('[data-test=portfolio-navigation] > :nth-child(1) a').should('have.class', 'active')
     cy.url().should('eq', `${Cypress.config().baseUrl}/?kategoria=zabawki#portfolio`)
-    cy.get('[data-test=portfolio-list]').children().its('length').should('eq', 3)
+    cy.get('[data-test=portfolio-list]').children().its('length').should('eq', portfolioToyItems)
   })
 
   it("can disable filter", () => {
@@ -17,7 +20,7 @@ describe("Portfolio", () => {
     cy.get('[data-test=portfolio-navigation] > :nth-child(1) a').click()
     cy.get('[data-test=portfolio-navigation] > :nth-child(1) a').should('not.have.class', 'active')
     cy.url().should('eq', `${Cypress.config().baseUrl}/#portfolio`)
-    cy.get('[data-test=portfolio-list]').children().its('length').should('eq', 9)
+    cy.get('[data-test=portfolio-list]').children().its('length').should('eq', portfolioItemsCount)
   })
 
   it("can change filter", () => {
