b68a56083e22c7ba83acd111ad24e36a0aa4443b
Tymoteusz Czech <mail@tymek.cz>
2021-06-17 22:39:27 +0200

    update title

--- src/components/HomePage/AboutMe/components/Title.tsx
+++ src/components/HomePage/AboutMe/components/Title.tsx
@@ -2,12 +2,11 @@ import React, { FC } from "react"
 
 const Title: FC = () => (
   <span className="leading-tight text-xl sm:text-xxl xl:text-xxl xxl:text-3xl font-bold tracking-wider">
-    <span className="inline-flex flex-wrap">
+    <span className="inline-flex flex-wrap z-10">
       <span className="order-1 md:order-1 mr-8">ilustracja</span>
       <span className="order-2 md:order-1 mr-8">
         gra<span className="tracking-normal">fi</span>ka
       </span>
-      <span className="order-1 md:order-1 mr-8">wzornictwo</span>
       <span className="order-1 md:order-1 mr-8">publikacja</span>
     </span>
   </span>
