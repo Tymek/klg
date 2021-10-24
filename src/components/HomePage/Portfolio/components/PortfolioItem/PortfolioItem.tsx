c7667922e4a65e4425fb6b711e40d59246c5021e
Tymoteusz Czech <mail@tymek.cz>
2021-10-24 13:57:26 +0100

    fix link hovers

--- src/components/HomePage/Portfolio/components/PortfolioItem/PortfolioItem.tsx
+++ src/components/HomePage/Portfolio/components/PortfolioItem/PortfolioItem.tsx
@@ -33,15 +33,9 @@ const PortfolioItem: FC<PortfolioItemProps> = ({
 }) => {
   const isTouch = useIsTouchDevice()
   const ref = useRef<HTMLLIElement>(null)
+  const linkRef = useRef<HTMLAnchorElement>(null)
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const [isHighlighted, setIsHighlighted] = useState<boolean>(false)
-  const open = () => {
-    setIsOpen(true)
-  }
-  const close = () => {
-    setIsOpen(false)
-  }
-
   useEffect(() => {
     if (ref.current && isTouch) {
       const observer = new IntersectionObserver(
@@ -70,18 +64,36 @@ const PortfolioItem: FC<PortfolioItemProps> = ({
     }
   }, [ref.current, isTouch])
 
+  useEffect(() => {
+    if (linkRef?.current) {
+      console.log(linkRef.current)
+      const open = () => setIsOpen(true)
+      const close = () => setIsOpen(false)
+
+      linkRef.current.addEventListener("mouseenter", open)
+      linkRef.current.addEventListener("focus", open)
+      linkRef.current.addEventListener("mouseleave", close)
+      linkRef.current.addEventListener("blur", close)
+      return () => {
+        console.log("destroy")
+        linkRef.current?.removeEventListener("mouseenter", open)
+        linkRef.current?.removeEventListener("focus", open)
+        linkRef.current?.removeEventListener("mouseleave", close)
+        linkRef.current?.removeEventListener("blur", close)
+      }
+    }
+    console.log(linkRef.current)
+  }, [linkRef.current])
+
   return (
     <li ref={ref} className={className} style={style}>
-      <div className="grid grid-cols-9 pb-16 lg:pb-0 lg:flex">
+      <div className="relative grid grid-cols-9 pb-16 lg:pb-0 lg:flex">
         <Link
+          ref={linkRef as any}
           to={link}
-          onMouseEnter={open}
-          onFocus={open}
-          onMouseLeave={close}
-          onBlur={close}
-          className="relative block sm:mr-32 md:mr-0 outline-none col-start-2 col-span-8 lg:w-full"
+          className="flex sm:mr-32 md:mr-0 outline-none col-start-2 col-span-8 lg:w-full"
         >
-          <div className="relative flex flex-col-reverse md:grid md:grid-cols-7 gap-x-15 gap-y-6 mx-auto lg:flex">
+          <div className="relative flex flex-col-reverse md:grid md:grid-cols-7 gap-x-15 gap-y-6 mx-auto lg:flex z-10">
             <div className="flex flex-col justify-center md:col-span-4 lg:w-full">
               <div className="font-bold uppercase text-lg xs:text-xl xl:text-2xl">
                 <SVGHoverText
